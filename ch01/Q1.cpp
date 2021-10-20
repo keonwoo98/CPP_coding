2aaead64e0450faac5534986b3d2d4b001a56a5b
keonwoo98 <keonwoo98@naver.com>
2021-10-20 23:01:32 +0900

    ch01-1: printf와 scanf를 대신하는 입출력 방식

---
+++ ch01/Q1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int			main(void)
+{
+	int		num;
+	int		add = 0;
+	
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout<<i + 1<<"번째 정수 입력: ";
+		std::cin>>num;
+		add += num;
+	}
+	std::cout<<"합계: "<<add<<std::endl;
+	return 0;
+}

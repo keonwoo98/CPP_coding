e9a920b29e784c2863523367f5f376a3f2ed6a6d
keonwoo98 <keonwoo98@naver.com>
2021-10-28 23:23:03 +0900

    ch01-1: printf와 scanf를 대신하는 입출력 방식

---
+++ ch01/ch01-1/Q2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int			main(void)
+{
+	char	name[20], p_num[20];
+
+	std::cout << "이름을 입력하세요: ";
+	std::cin >> name;
+
+	std::cout << "전화번호를 입력하세요: ";
+	std::cin >> p_num;
+
+	std::cout << "이름: " << name << std::endl;
+	std::cout << "전화번호: " << p_num << std::endl;
+	return 0;
+}

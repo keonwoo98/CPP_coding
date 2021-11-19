2626725680d867509d3c48c74057b0befa557da2
keonwoo98 <keonwoo98@naver.com>
2021-11-19 09:31:46 +0900

    ch01-6: OOP 단계별 프로젝트 01단계

--- ch01/ch01-6/BankingSystemVer01.cpp
+++ ch01/ch01-6/BankingSystemVer01.cpp
@@ -41,6 +41,7 @@ int			main(void)
 		{
 			case MAKE :
 				MakeAccount();
+				break ;
 			case DEPOSIT :
 				DepositMoney();
 				break ;
@@ -57,4 +58,89 @@ int			main(void)
 		}
 	}
 	return (0);
+}
+
+void		ShowMenu(void)
+{
+	cout << "------Menu------" << endl;
+	cout << "1. 계좌개설" << endl;
+	cout << "2. 입 금" << endl;
+	cout << "3. 출 금" << endl;
+	cout << "4. 계좌정보 전체 출력" << endl;
+	cout << "5. 프로그램 종료" << endl;
+}
+
+void		MakeAccount(void)
+{
+	int		id;
+	char	name[NAME_LEN];
+	int		balance;
+
+	cout << "[계좌개설]" << endl;
+	cout << "계좌 ID : "; cin >> id;
+	cout << "이름 : "; cin >> name;
+	cout << "입금액 : "; cin >> balance;
+	cout << endl;
+
+	accArr[accNum].accID = id;
+	accArr[accNum].balance = balance;
+	strcpy(accArr[accNum].cusName, name);
+	accNum++;
+}
+
+void		DepositMoney(void)
+{
+	int		money;
+	int		id;
+
+	cout << "[입 금]" << endl;
+	cout << "계좌 ID : "; cin >> id;
+	cout << "입금액 : "; cin >> money;
+
+	for (int i = 0; i < accNum; i++)
+	{
+		if (accArr[i].accID == id)
+		{
+			accArr[i].balance += money;
+			cout << "입금완료" << endl << endl;
+			return ;
+		}
+	}
+	cout << "유효하지 않은 ID 입니다" << endl << endl;
+}
+
+void		WithdrawMoney(void)
+{
+	int		money;
+	int		id;
+
+	cout << "[출 금]" << endl;
+	cout << "계좌 ID : "; cin >> id;
+	cout << "출금액 : "; cin >> money;
+
+	for (int i = 0; i < accNum; i++)
+	{
+		if (accArr[i].accID == id)
+		{
+			if (accArr[i].balance < money)
+			{
+				cout << "잔액부족" << endl << endl;
+				return ;
+			}
+			accArr[i].balance -= money;
+			cout << "출금완료" << endl << endl;
+			return ;
+		}
+	}
+	cout << "유효하지 않은 ID 입니다" << endl << endl;
+}
+
+void		ShowAllAccInfo(void)
+{
+	for (int i = 0; i < accNum; i++)
+	{
+		cout << "계좌 ID : " << accArr[i].accID << endl;
+		cout << "이름 : " << accArr[i].cusName << endl;
+		cout << "잔액 : " << accArr[i].balance << endl << endl;
+	}
 }

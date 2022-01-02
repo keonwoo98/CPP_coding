923d48a428c27fe79d4d44c86f12e1d840cdf1a3
keonwoo98 <keonwoo98@naver.com>
2022-01-02 21:47:33 +0900

    ch04-2: 캡슐화(Encapsulation)

--- ch04/ch04-2/Q1.cpp
+++ ch04/ch04-2/Q1.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+class Point
+{
+private :
+    int xpos, ypos;
+public :
+    void    Init(int x, int y)
+    {
+        xpos = x;
+        ypos = y;
+    }
+    void    ShowPointInfo() const
+    {
+        cout << "[" << xpos << ", " << ypos << "]" << endl;
+    }
+};

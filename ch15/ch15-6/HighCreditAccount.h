0c721ca7e4845a76c9ef75789385d42a0975193b
keonwoo98 <keonwoo98@naver.com>
2022-06-04 21:36:11 +0900

    ch15-6: OOP 단계별 프로젝트 11단계

---
+++ ch15/ch15-6/HighCreditAccount.h
@@ -0,0 +1,26 @@
+#ifndef __HIGHCREDIT_ACCOUNT_H__
+#define __HIGHCREDIT_ACCOUNT_H__
+
+#include "NormalAccount.h"
+#include "String.h"
+#include "AccountException.h"
+
+class HighCreditAccount : public NormalAccount
+{
+private :
+    int specialRate;
+public :
+    HighCreditAccount(int ID, int money, String name, int rate, int special)
+        : NormalAccount(ID, money, name, rate), specialRate(special)
+    { }
+    virtual void Deposit(int money)
+    {
+        if (money < 0)
+		    throw MinusException(money); 
+        
+        NormalAccount::Deposit(money);
+        Account::Deposit(money * (specialRate / 100.0));
+    }
+};
+
+#endif
